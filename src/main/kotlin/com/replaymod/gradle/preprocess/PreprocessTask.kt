bf1a9e0651661515c4d6c3b39c8e0f5418705f54
Deftu <me@deftu.dev>
2026-01-15 07:08:56 +0200

    Replace usage of utils internal to Kotlin compiler (from upstream)

--- src/main/kotlin/com/replaymod/gradle/preprocess/PreprocessTask.kt
+++ src/main/kotlin/com/replaymod/gradle/preprocess/PreprocessTask.kt
@@ -18,9 +18,6 @@ import org.gradle.api.model.ObjectFactory
 import org.gradle.api.tasks.*
 import org.gradle.kotlin.dsl.mapProperty
 import org.gradle.kotlin.dsl.property
-import org.jetbrains.kotlin.backend.common.peek
-import org.jetbrains.kotlin.backend.common.pop
-import org.jetbrains.kotlin.backend.common.push
 import org.slf4j.LoggerFactory
 import java.io.File
 import java.io.Serializable
@@ -790,7 +787,7 @@ class CommentPreprocessor(
                         originalLine
                     }
                 } else {
-                    val currIndent = indentStack.peek()!!
+                    val currIndent = indentStack.last()
                     if (trimmed.isEmpty()) {
                         currIndent + kws.eval
                     } else if (!trimmed.startsWith(kws.eval) && currIndent.length <= line.indentation.length) {
@@ -815,7 +812,7 @@ class CommentPreprocessor(
             mapped
         }.also {
             if (stack.isNotEmpty()) {
-                throw ParserException("Missing endif on line ${stack.peek()?.lineno} of $fileName")
+                throw ParserException("Missing endif on line ${stack.last().lineno} of $fileName")
             }
         }
     }
@@ -847,3 +844,6 @@ class CommentPreprocessor(
 
     class ParserException(str: String) : RuntimeException(str)
 }
+
+private fun <E> MutableList<E>.push(e: E) = add(e)
+private fun <E> MutableList<E>.pop() = removeLast()
